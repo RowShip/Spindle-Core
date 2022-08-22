61e5a4ea5d6624074edc502d90d7168da602fbe6
gurucrypto23 <justinliu0623@gmail.com>
2022-08-22 07:01:32 -0700

    update the Resolver contract

--- contracts/Resolver.sol
+++ contracts/Resolver.sol
@@ -3,35 +3,44 @@ import "./SSUniVault.sol";
 pragma solidity ^0.8.4;
 
 contract Resolver {
+
+    // contract of SSNUniVault
     SSUniVault public immutable SWAP_SWEEP;
 
     constructor(address _swapSweep) {
+        // Set execution contract address
         SWAP_SWEEP = SSUniVault(_swapSweep);
     }
 
-    // function checkCanRebalance(uint256 _deadline)
-    //     external
-    //     view
-    //     returns (bool canExec, bytes memory execPayload)
-    // {
-    //     // solhint-disable not-rely-on-time
-    //     canExec = SWAP_SWEEP.canRebalance();
+    function checkCanRebalance(uint256 _deadline)
+        external
+        view
+        returns (bool canExec, bytes memory execPayload)
+    {
+        // solhint-disable not-rely-on-time
+        // set the Execution
+        canExec = SWAP_SWEEP.canRebalance();
 
-    //     _deadline = block.timestamp + _deadline;
-    //     execPayload = abi.encodeWithSelector(
-    //         SWAP_SWEEP.rebalance.selector,
-    //         _deadline
-    //     );
-    // }
+        // Reset tje payload
+        execPayload = abi.encodeWithSelector(
+            SWAP_SWEEP.rebalance.selector,
+            block.timestamp + _deadline
+        );
+        return (canExec, execPayload);
+    }
 
-    // function checkCanReposition()
-    //     external
-    //     view
-    //     returns (bool canExec, bytes memory execPayload)
-    // {
-    //     // solhint-disable not-rely-on-time
-    //     canExec = SWAP_SWEEP.canReposition();
+    function checkCanReposition()
+        external
+        view
+        returns (bool canExec, bytes memory execPayload)
+    {
+        // solhint-disable not-rely-on-time
+        // set the Execution
+        canExec = SWAP_SWEEP.canReposition();
 
-    //     execPayload = abi.encodeWithSignature("reposition()");
-    // }
+        // Reset tje payload
+        execPayload = abi.encodeWithSignature("reposition()");
+        return (canExec, execPayload);
+
+    }
 }
